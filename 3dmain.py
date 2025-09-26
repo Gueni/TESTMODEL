a77a3a6d1542dcf100742adec75537b839abfb42
Mohamed Gueni | محمد قاني <20975550+Gueni@users.noreply.github.com>
2025-09-26 13:10:19 +0100

    Add new CSV maps for peak currents, peak voltages, RMS currents, RMS voltages, temperature readings, and thermal statistics
    - Created Peak_Currents_Map.csv with 4264 entries of peak current data.
    - Created Peak_Voltages_Map.csv with 3644 entries of peak voltage data.
    - Created RMS_Currents_Map.csv with 4300 entries of RMS current data.
    - Created RMS_Voltages_Map.csv with 3628 entries of RMS voltage data.
    - Created Temps_Map.csv with 292 entries of temperature data.
    - Created Thermal_Stats_Map.csv with 576 entries of thermal statistics data.

--- 3dmain.py
+++ 3dmain.py
@@ -1,8 +1,10 @@
 
-
+#?-------------------------------------------------------------------------------------------------------------------------------
+#?  Import Libraries
+#?-------------------------------------------------------------------------------------------------------------------------------
 import os, json
 from natsort import natsorted
-
+import pandas as pd, numpy as np
 
 #?-------------------------------------------------------------------------------------------------------------------------------
 #?  Header Files to JSON Array
@@ -11,13 +13,25 @@ from natsort import natsorted
 # Excluded files: 'header.json', 'FFT_Current.json', 'FFT_Voltage.json'
 # j_array will contain the combined data
 
-f_path     = r"D:\WORKSPACE\BJT-MODEL\assets\HEADER_FILES"
-all_files  = os.listdir(f_path)
-j_files    = natsorted([f for f in all_files if f not in {'header.json', 'FFT_Current.json', 'FFT_Voltage.json'}])
-j_array    = []
+f_path      = r"D:\WORKSPACE\BJT-MODEL\assets\HEADER_FILES"
+all_files   = os.listdir(f_path)
+j_files     = natsorted([f for f in all_files if f not in {'header.json', 'FFT_Current.json', 'FFT_Voltage.json','Peak_Losses.json'}])
+j_array     = []
 [j_array.extend(data) if isinstance((data := json.load(open(os.path.join(f_path, f)))), list) else j_array.append(data) for f in j_files]
 
 # print(f"Processed {len(j_files)} files")
 # print(f"Combined array length: {len(j_array)}")
 # print("Array contents:", j_array)
 #?-------------------------------------------------------------------------------------------------------------------------------
+
+# Define the folder containing CSV files and combine them into a single CSV file
+folder      = r"D:\WORKSPACE\BJT-MODEL\CSV_MAPS"
+
+# Load and combine CSV files, excluding specific ones
+# Excluded files: 'FFT_Voltage_Map.csv', 'FFT_Current_Map.csv'
+# The combined_matrix will contain the horizontally stacked data from the CSV files
+combined_matrix = np.hstack([pd.read_csv(os.path.join(folder, f), header=None).values 
+                           for f in sorted(os.listdir(folder)) 
+                           if f.endswith('.csv') and f not in ['FFT_Voltage_Map.csv', 'FFT_Current_Map.csv']])
+
+# print(f"Combined matrix shape: {combined_matrix.shape}")
