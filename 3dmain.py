89142c637335f50cea4f57adfd733d2962667585
Mohamed Gueni | محمد قاني <20975550+Gueni@users.noreply.github.com>
2025-09-27 16:35:53 +0100

    Add FFT suffix to current and voltage measurement labels in JSON files
    - Updated FFT_Current.json to include "FFT" suffix for all current measurement labels.
    - Updated FFT_Voltage.json to include "FFT" suffix for all voltage measurement labels.

--- 3dmain.py
+++ 3dmain.py
@@ -10,109 +10,89 @@ import re
 import plotly.graph_objects as go
 import datetime
 from plotly.io import to_html
-
 #?-------------------------------------------------------------------------------------------------------------------------------
 #?  Define Variables
 #?-------------------------------------------------------------------------------------------------------------------------------
-f_path                                          = r"D:\WORKSPACE\BJT-MODEL\assets\HEADER_FILES"
-all_files                                       = os.listdir(f_path)
-j_array  ,list_of_plots                         = [], []
+header_path                                     = r"D:\WORKSPACE\BJT-MODEL\assets\HEADER_FILES"
+all_header_files                                = os.listdir(header_path)
+headers_array  ,list_of_plots, fft_plots        = [], [], []
 folder                                          = r"D:\WORKSPACE\BJT-MODEL\CSV_MAPS"
 excluded                                        = ['FFT_Voltage_Map.csv', 'FFT_Current_Map.csv']
+excluded_headers                                = {'header.json', 'Peak_Losses.json'}
 input_json                                      = r"D:\WORKSPACE\BJT-MODEL\assets\Input_vars.json"
 with open(input_json) as f:config               = json.load(f)
 var1, var2                                      = config["Var1"], config["Var2"]
-split_3D                                        = False  
+split_3D                                        = True  
 base64_file                                     = r"D:\WORKSPACE\BJT-MODEL\assets\BMW_Base64_Logo.txt"
 html_base                                       = r"D:\WORKSPACE\BJT-MODEL\results\result" 
 script_name                                     = os.path.basename(__file__)
 UTC                                             = int(datetime.datetime.now(datetime.timezone.utc).timestamp())
 date                                            = str(datetime.datetime.now().replace(microsecond=0))
 with open(base64_file, 'r') as f: base64_img    = ''.join(line.strip() for line in f)
+harmonics                                       = [1,2] 
+F_fund                                          = 1e5 
 #?-------------------------------------------------------------------------------------------------------------------------------
 #?  Header Files to JSON Array
 #?-------------------------------------------------------------------------------------------------------------------------------
-j_files     = natsorted([f for f in all_files if f not in {'header.json', 'FFT_Current.json', 'FFT_Voltage.json','Peak_Losses.json'}])
-[j_array.extend(data) if isinstance((data := json.load(open(os.path.join(f_path, f)))), list) else j_array.append(data) for f in j_files]
-# print(f"Processed {len(j_files)} files")
-# print(f"Combined array length: {len(j_array)}")
-# print("Array contents:", j_array)
+headers_files   = natsorted([f for f in all_header_files if f not in excluded_headers])
+headers_lists   = [data if isinstance((data := json.load(open(os.path.join(header_path, f)))), list) else [data] for f in headers_files]
+FFT_headers     = sum(headers_lists[5:7], [])
+headers_array   = sum(headers_lists[:5] + headers_lists[7:], [])
 #?-------------------------------------------------------------------------------------------------------------------------------
 #?  Combine CSV Files into a Single Matrix
 #?-------------------------------------------------------------------------------------------------------------------------------
-# Load and combine CSV files, excluding specific ones
-# The combined_matrix will contain the horizontally stacked data from the CSV files
 combined_matrix = np.hstack(                                                            #   Horizontally stack arrays
                             [pd.read_csv(os.path.join(folder, f), header=None).values   #   Read each CSV file without headers
                             for f in sorted(os.listdir(folder))                         #   Sort files alphabetically
                             if f.endswith('.csv') and f not in excluded]                #   Exclude specific files
                             )
-# print(f"Combined matrix shape: {combined_matrix.shape}")
-# print("Combined matrix contents:", combined_matrix)
-# print("column_5:", combined_matrix[:, j_array.index("CT Trafo Primary Current AVG")])
+
+combined_fft_matrix = np.hstack(
+                                [pd.read_csv(os.path.join(folder, f), header=None).values 
+                                for f in natsorted([f for f in os.listdir(folder) 
+                                if f.endswith('.csv') and f in excluded])]
+                            )
 #?-------------------------------------------------------------------------------------------------------------------------------
 #?  Find Sweep Variables and Their Combinations
 #?-------------------------------------------------------------------------------------------------------------------------------
-# Extract sweep variables (keys matching "X" followed by digits)
 sweep_vars = {k: eval(v) for k, v in config.items() if re.fullmatch(r"X\d+", k) and eval(v) != [0]}
-# print("Sweep Variables:", sweep_vars)
-# Generate all combinations of sweep variables that have more than one value
-# Lexicographic order (default of itertools.product)
-all_combos  = list(product(*[vals for vals in sweep_vars.values() if vals != [0]]))  # each row = one simulation case
-# print("All Combinations:", all_combos)
-#?-------------------------------------------------------------------------------------------------------------------
-#? Automatically determine fixed variables
-#?-------------------------------------------------------------------------------------------------------------------
-# Extract all sweep variables that are not Var1 or Var2 and not [0]
-other_vars = {k: eval(v) for k, v in config.items()
-              if re.fullmatch(r"X\d+", k)         # X variables only
-              and k not in [config["Var1"], config["Var2"]]
-              and eval(v) != [0]                  # ignore zero lists
-              and not config["sweepNames"][int(re.search(r'\d+', k).group())-1].startswith("X") # real name
-             }
-
-# Generate all combinations of these fixed variables
-fixed_combos = list(product(*other_vars.values()))
-
-# Generate a list of their keys in order
-fixed_keys = list(other_vars.keys())
+all_combos = list(product(*sweep_vars.values()))
+fft_combos = list(map(tuple, np.repeat(all_combos, len(harmonics), axis=0).astype(object)))
 #?-------------------------------------------------------------------------------------------------------------------
 #? Select Rows Based on Automatically Determined Fixed Variables
 #?-------------------------------------------------------------------------------------------------------------------
-sweep_keys = list(sweep_vars.keys())                             # all sweep variable keys
-
-# Identify other sweep variables that will act as "fixed" for plotting
+sweep_keys = list(sweep_vars.keys())                            
 other_vars = {k: eval(v) for k, v in config.items()
-              if re.fullmatch(r"X\d+", k)                     # X variables only
-              and k not in [var1, var2]                       # exclude Var1 and Var2
-              and eval(v) != [0]                               # ignore zero lists
-              and not config["sweepNames"][int(re.search(r'\d+', k).group())-1].startswith("X") # real name
+              if re.fullmatch(r"X\d+", k)                    
+              and k not in [var1, var2]                    
+              and eval(v) != [0]                       
+              and not config["sweepNames"][int(re.search(r'\d+', k).group())-1].startswith("X") 
              }
-
-# Generate all combinations of these fixed variables
 fixed_combos = list(product(*other_vars.values()))
 fixed_keys   = list(other_vars.keys())
 
-# Loop over each fixed combination to select rows for plotting
 for fixed_values in fixed_combos:
-    fixed = dict(zip(fixed_keys, fixed_values))  # create fixed dictionary for this iteration
+    fixed = dict(zip(fixed_keys, fixed_values)) 
     try :
-        # select rows matching this fixed combination
         rows = np.array([
             (i, combo[sweep_keys.index(var1)], combo[sweep_keys.index(var2)])
             for i, combo in enumerate(all_combos)
             if all(combo[sweep_keys.index(k)] == v for k, v in fixed.items())
         ])
-    except: len(rows) == 0; print("No matching rows found for fixed combination:", fixed)
-
-# print("Selected Rows:", rows)
+        fft_rows = np.array([
+            (i, combo[sweep_keys.index(var1)], combo[sweep_keys.index(var2)])
+            for i, combo in enumerate(fft_combos)
+            if all(combo[sweep_keys.index(k)] == v for k, v in fixed.items())
+        ])
+    except: 
+        len(rows) == 0 or len(fft_rows) == 0; print("No matching rows found for fixed combination:", fixed)
 #?-------------------------------------------------------------------------------------------------------------------------------
 #?  Plotting the 3D Surface
 #?-------------------------------------------------------------------------------------------------------------------------------
-for component, fixed_values in product(j_array, fixed_combos):
+for component, fixed_values in product(headers_array, fixed_combos):
 
         fixed           = dict(zip(fixed_keys, fixed_values))
-        z_column        = j_array.index(component)
+        z_column        = headers_array.index(component)
         x_vals          = rows[:, 1]
         y_vals          = rows[:, 2]
         z_vals          = combined_matrix[rows[:, 0].astype(int), z_column]
@@ -121,24 +101,99 @@ for component, fixed_values in product(j_array, fixed_combos):
         fixed_title     = " | ".join(f"{config['sweepNames'][int(re.search(r'\d+', k).group())-1]} = {v}" for k, v in fixed.items())
         plot_type       = "2D" if sum(1 for vals in sweep_vars.values() if len(vals) > 1 and vals != [0]) < 3 else "3D"
 
-        match plot_type:
-            case "2D":
-                fig = go.Figure()
-                fig.add_trace(go.Scatter(x=x_vals,y=z_vals,mode='lines',fill='tozeroy',line=dict(color='royalblue'),name=component))
-                fig.update_layout(title=f"{component}  @ {fixed_title}",xaxis_title=var1_name,yaxis_title=component)
-            case "3D":
-                x_unique  = np.unique(x_vals)
-                y_unique  = np.unique(y_vals)
-                X, Y      = np.meshgrid(x_unique, y_unique)
-                Z         = np.full_like(X, fill_value=np.nan, dtype=float)
-                xi        = np.searchsorted(x_unique, x_vals)
-                yi        = np.searchsorted(y_unique, y_vals)
-                Z[yi, xi] = z_vals
-                fig       = go.Figure(data=[go.Surface(x=X,y=Y,z=Z,colorscale='Viridis',colorbar=dict(title=component))])
-                fig.update_layout(  title=f'{component} @ {fixed_title}',
+        x_unique  = np.unique(x_vals)
+        y_unique  = np.unique(y_vals)
+        X, Y      = np.meshgrid(x_unique, y_unique)
+        Z         = np.full_like(X, fill_value=np.nan, dtype=float)
+        xi        = np.searchsorted(x_unique, x_vals)
+        yi        = np.searchsorted(y_unique, y_vals)
+        Z[yi, xi] = z_vals
+        fig       = go.Figure(data=[go.Surface(x=X,y=Y,z=Z,colorscale='Viridis',colorbar=dict(title=component))])
+        fig.update_layout(  title=f'{component} @ {fixed_title}',
                                     scene=dict(xaxis_title=var1_name,yaxis_title=var2_name,zaxis_title=component)
                                 )
         list_of_plots.append(fig)
+#?-------------------------------------------------------------------------------------------------------------------------------
+#?  FFT Plotting the 2D/3D Bar Charts
+#?-------------------------------------------------------------------------------------------------------------------------------
+
+def fft_barchart3d(x_vals, y_vals, z_vals, title, z_title, colorscale='Viridis', opacity=0.8):
+
+    fig = go.Figure()
+    ann = []
+
+    # Compute dynamic bar thickness based on minimum spacing
+    if len(x_vals) > 1:
+        dx = np.min(np.diff(np.sort(np.unique(x_vals)))) * 0.4
+    else:
+        dx = 1.0
+    if len(y_vals) > 1:
+        dy = np.min(np.diff(np.sort(np.unique(y_vals)))) * 0.4
+    else:
+        dy = 1.0
+
+    for i, z_max in enumerate(z_vals):
+        x_cnt, y_cnt = x_vals[i], y_vals[i]
+        x_min, x_max = x_cnt - dx/2, x_cnt + dx/2
+        y_min, y_max = y_cnt - dy/2, y_cnt + dy/2
+
+        # Add Mesh3d bar
+        fig.add_trace(go.Mesh3d(
+            x=[x_min, x_min, x_max, x_max, x_min, x_min, x_max, x_max],
+            y=[y_min, y_max, y_max, y_min, y_min, y_max, y_max, y_min],
+            z=[0, 0, 0, 0, z_max, z_max, z_max, z_max],
+            alphahull=0,
+            intensity=[0, 0, 0, 0, z_max, z_max, z_max, z_max],
+            coloraxis='coloraxis',
+            opacity=opacity
+        ))
+
+        # Annotation on top
+        ann.append(dict(
+            showarrow=False,
+            x=x_cnt, y=y_cnt, z=z_max,
+            text=f'{z_max:.2f}',
+            font=dict(color='white', size=10),
+            bgcolor='rgba(0,0,0,0.3)',
+            xanchor='center', yanchor='middle'
+        ))
+
+    fig.update_layout(
+        title=title,
+        scene=dict(
+            xaxis_title="Frequency [Hz]",
+            yaxis_title="Sweep Var",
+            zaxis_title=z_title,
+            annotations=ann
+        ),
+        coloraxis=dict(colorscale=colorscale)    )
+
+    return fig
+
+for component, fixed_values in product(FFT_headers, fixed_combos):
+    fixed       = dict(zip(fixed_keys, fixed_values))
+    z_column    = FFT_headers.index(component)
+    y_vals      = fft_rows[:, 2]
+    harm_xval   = np.array(harmonics) * F_fund 
+    x_vals      = np.tile(harm_xval, int(np.ceil(len(y_vals) / len(harm_xval))))[:len(y_vals)]
+    z_vals      = combined_fft_matrix[fft_rows[:, 0].astype(int), z_column]
+    var1_name   = config["sweepNames"][int(re.search(r'\d+', config["Var1"]).group())-1]
+    var2_name   = config["sweepNames"][int(re.search(r'\d+', config["Var2"]).group())-1]
+    fixed_title = " | ".join(f"{config['sweepNames'][int(re.search(r'\d+', k).group())-1]} = {v}" for k, v in fixed.items())
+    x_unique    = np.unique(x_vals)
+    y_unique    = np.unique(y_vals)
+    X, Y        = np.meshgrid(x_unique, y_unique)
+    Z           = np.full_like(X, fill_value=np.nan, dtype=float)
+    xi          = np.searchsorted(x_unique, x_vals)
+    yi          = np.searchsorted(y_unique, y_vals)
+    Z[yi, xi]   = z_vals
+    Xf          = X.flatten()
+    Yf          = Y.flatten()
+    Zf          = Z.flatten()
+    fig         = fft_barchart3d(x_vals, y_vals, z_vals, f"{component} @ {fixed_title}", component)
+
+    fft_plots.append(fig)
+
 #?-------------------------------------------------------------------------------------------------------------------------------
 #?  Generate HTML Report 
 #?-------------------------------------------------------------------------------------------------------------------------------
@@ -166,9 +221,14 @@ def write_html_report(html_file, plots):
         f.write('<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>\n')
 
 if split_3D:
-    for idx, component in enumerate(j_array):
+    for idx, component in enumerate(headers_array):
         start, end      = idx * len(fixed_combos), (idx + 1) * len(fixed_combos)
         write_html_report(f"{html_base}_{UTC}_{component}.html", list_of_plots[start:end])
+
+    for idx, component in enumerate(FFT_headers):
+        start, end      = idx * len(fixed_combos), (idx + 1) * len(fixed_combos)
+        write_html_report(f"{html_base}_{UTC}_{component}.html", fft_plots[start:end])
 else:
     write_html_report(f"{html_base}_{UTC}.html", list_of_plots)
+    write_html_report(f"{html_base}_FFT_{UTC}.html", fft_plots)
 #?-------------------------------------------------------------------------------------------------------------------------------
