e2793df30feb388553d13d18ecb12474b772b525
Mohamed Gueni | محمد قاني <20975550+Gueni@users.noreply.github.com>
2025-10-25 14:41:24 +0100

    Enhance create_parameter_table function to support configurable dropdown levels and improve path retrieval logic for nested dictionaries

--- write_html_report.py
+++ write_html_report.py
@@ -1,16 +1,2 @@
-# Your original approach adapted for templates
-plot_items = ""
-
-for i in range(len(figures)):
-    # Add the plot
-    plot_items += figures[i].to_html(full_html=False, include_plotlyjs=False)
-    
-    # Add comment if conditions are met
-    if dp.JSON["figureName"] and dp.JSON["figureComment"]:
-        if not comments[i] == " ":
-            plot_items += f'<input type="text" class="comment-box" value="{comments[i]}" readonly="readonly">'
-    
-    # Add separator (you can style this in CSS)
-    plot_items += '<div class="separator"></div>'
-
-# Then pass plot_items to your template
+        import re
+        html_content = re.sub(r'<div[^>]*class="download_section"[^>]*>.*?</div>', '', html_content, flags=re.DOTALL)
