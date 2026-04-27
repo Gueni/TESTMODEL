f8ec4707a1eea13e51aa7714aec9921a5782b9d6
Mohamed Gueni <m0hamed.gu3ni@gmail.com>
2026-04-27 17:44:17 +0100

    Enhance update_signal_selector to exclude specified indices from new calculations

--- testK.py
+++ testK.py
@@ -10,18 +10,13 @@ def parse_indices(indices_str):
             indices.append(int(token))
     return indices
 
-def update_signal_selector(indices_str, rail, n_signals=36):
+def update_signal_selector(indices_str, rail, n_signals=36, exclude=[]):
     indices = parse_indices(indices_str)
-    new_indices = [i + (rail - 1) * n_signals for i in indices]
+    new_indices = [i + (rail - 1) * n_signals for i in indices if i not in exclude]
     combined = indices + new_indices
     print("[" + " ".join(str(i) for i in combined) + "]")
 
 # Examples
-update_signal_selector("[1 7 13]", rail=2, n_signals=36)
-# [1 7 13 37 43 49]
-
-update_signal_selector("[25:36]", rail=2, n_signals=36)
-# [25 26 27 28 29 30 31 32 33 34 35 36 61 62 63 64 65 66 67 68 69 70 71 72]
-
-update_signal_selector("[1 7 25:30 36]", rail=2, n_signals=36)
-# [1 7 25 26 27 28 29 30 36 37 43 61 62 63 64 65 66 67 72]
+update_signal_selector("[1 7 13 25:30]", rail=2, n_signals=36, exclude=[7])
+# 7 is common, won't be duplicated
+# [1 7 13 25 26 27 28 29 30 37 43 61 62 63 64 65 66]
